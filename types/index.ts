52c24bf2e901cde44a532cd840fe81ee5324ebdd
YoyoBounce99 <lleul.teferi@gmail.com>
2025-11-20 18:29:52 +0300

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,17 @@
+export interface Question {
+  id: number;
+  question_text: string;
+  image_url?: string;
+  option_a: string;
+  option_b: string;
+  option_c: string;
+  option_d: string;
+  correct_option: 'A' | 'B' | 'C' | 'D';
+  explanation: string;
+}
 
+export interface Profile {
+  id: string;
+  email: string;
+  paid_until: string | null;
+}
